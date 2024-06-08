fc84fe962c3a3e44b064f93775d65e91a7dfc6d8
willwellwu <wujh1123@163.com>
2024-06-08 22:07:52 +0800

    dataset_modi

--- utils/dataset_modi.py
+++ utils/dataset_modi.py
@@ -24,27 +24,3 @@ class ScaledDataSet(DataSet):
         assert len(self.scales) == targets.shape[1]
         return loss_fn(targets * self.scales, outputs * self.scales)
 
-
-class ScaledComponentDataSet(DataSet):
-    def __init__(
-        self,
-        X_train=None,
-        y_train=None,
-        X_test=None,
-        y_test=None,
-        fname_train=None,
-        fname_test=None,
-        col_x=None,
-        col_y=None,
-        standardize=False,
-        scales=(1.0, ),
-        components=(0, ),
-    ):
-        super().__init__(X_train, y_train, X_test, y_test, fname_train, fname_test, col_x, col_y, standardize)
-        assert len(scales) == len(components)
-        self.scales = scales
-        self.components = components
-
-    def losses(self, targets, outputs, loss_fn, inputs, model, aux=None):
-        return loss_fn(targets[:, self.components] * self.scales, outputs[:, self.components] * self.scales)
-
